35d2761b54caa0bff756814061c1bf1292122f2f
hi <hi@HI-PC.local>
2020-07-21 12:30:57 +0900

    Update .babelrc, .editorconfig, and 37 more files...

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Header />
+    <Visual />
+    <router-view />
+    <Link />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Visual from "@/components/Visual";
+import Link from "@/components/Link";
+import Footer from "@/components/Footer";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Visual,
+    Link,
+    Footer
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
